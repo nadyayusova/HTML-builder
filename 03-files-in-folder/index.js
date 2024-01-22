18cb38b29ac32511c260b74898bc25aa968c5364
nadyayusova <60035655+nadyayusova@users.noreply.github.com>
2024-01-22 10:46:40 +0300

    feat: add third task solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const fsPromise = require('fs/promises');
+
+const targetDir = path.join(__dirname, 'secret-folder');
+
+fsPromise.readdir(targetDir)
+  .then((files) => {
+    files.forEach((file) => {
+      const filePath = path.join(targetDir, file);
+
+      fsPromise.lstat(filePath)
+        .then((stat) => {
+          if (stat.isFile()) {
+            const fileData = path.parse(path.join(targetDir, file));
+
+            console.log(`${fileData.name} - ${fileData.ext.slice(1)} - ${(stat.size / 1024).toFixed(2)} kb`);
+          }
+        });
+    });
+  })
+  .catch((err) => console.log(err));
